268e67eefa5a3e0a74844fe121e95c984fd38bd0
Alireza <94902455+AlirezaSiavashi@users.noreply.github.com>
2026-07-06 14:04:53 +0200

    Update mult_rtl.vhd to improve architecturelast2
    Refactor multiplier RTL description and testbench structure.

--- prak_2/vhd/mult_rtl.vhd
+++ prak_2/vhd/mult_rtl.vhd
@@ -1,111 +1,74 @@
 library IEEE;
-use IEEE.std_logic_1164.all; 
-use IEEE.math_real.all; 
-use IEEE.numeric_std.all; 
+use IEEE.std_logic_1164.all; -- import std_logic types
+use IEEE.numeric_std.all; -- for type conversion to_unsigned
+
+--library STD;
+--use STD.textio.all;
 
 --------------------------------------------------------------------------------
---!@file: TB_mult_rtl.vhd
---!@brief: testbench for the rtl description of the multiplier
+--!@file: mult_rtl.vhd
+--!@brief: this is a simple multiplier description (generic) and iterativ
 --!...
 --
 --!@author: Tobias Koal(TK)
 --!@revision info :
 -- last modification by tkoal(TK)
--- Mon Mar  4 09:49:47 CET 2013
+-- Fri Jul  1 15:46:21 CEST 2011
 --------------------------------------------------------------------------------
 
 -- entity description
 
-entity TB_mult_rtl is
+entity mult_rtl is
+port(
+		a, b	:	IN	unsigned(19 downto 0);
+		z		:	OUT	unsigned(39 downto 0);
+		clk, reset : IN std_logic;
+		enable	:	IN	std_logic;
+		ready	:	OUT	std_logic
+);
 end entity;
 
 -- architecture description
 
-architecture testbench of TB_mult_rtl is
+architecture behave of mult_rtl is
 
-
-	-- CONSTANTS (upper case only!)
-	CONSTANT CYCLE_TIME  : time := 1.0 ns;
-	CONSTANT RESET_TIME  : time := 4*CYCLE_TIME + CYCLE_TIME / 5.0;
-
-	-- SIGNALS (lower case only!)
-	signal s_a, s_b						:	unsigned(19 downto 0) := (others => '0');
-	signal s_z,	s_z_golden, s_z_golden_buf				:	unsigned(39 downto 0) := (others => '0');
-	signal reset, clk, ready, enable	:	std_logic;
 
 begin
 
-	-- put your code here!
-	tb_component : entity work.mult_rtl(behave)
-	port map (
-				a => s_a,
-				b => s_b,
-				z => s_z,
-				clk => clk,
-				reset => reset,
-				ready => ready,
-				enable => enable
-		  );
-
-
-	-- create a stimulus process here!
-	stimulus : process is
-		variable seed1,seed2 : positive := 1;
-		variable a_real, b_real : real := 0.0;
+	mult_proc : process (clk,reset)
+		variable z_intern, a_intern :	unsigned(39 downto 0);
+		variable a_buf, b_buf		:	unsigned(19 downto 0);
+		variable count				:	natural;
 	begin
-		enable <= '0';
-		wait for RESET_TIME;
-		loop
-			wait until ready = '1';
-			-- create random numbers
-			uniform (seed1, seed2, a_real);
-			uniform (seed1, seed2, b_real);
-			-- convert random number from real to std_logic_vector
-			s_a <= to_unsigned(integer(a_real * 2.0**20),20);
-			s_b <= to_unsigned(integer(b_real * 2.0**20),20);
-			wait for CYCLE_TIME/2;
-			enable <= '1';
-			wait for CYCLE_TIME;
-			enable <= '0';
-		end loop;
+		if reset = '1' then
+			ready 		<= '1';
+			z 			<= (others => '0');
+			z_intern	:=	(others => '0');
+			a_intern	:=	(others => '0');
+			a_buf		:=	(others => '0');
+			b_buf		:=	(others => '0');
+			count		:=	0;
+		elsif rising_edge(clk) then 
+			if enable = '1' then
+				z_intern := (others => '0');
+				count	:= 0;
+				a_buf	:= a;
+				b_buf	:= b;
+				ready	<= '0';
+			elsif count < 20 then
+				ready	<= '0';
+				if b_buf(count) = '1' then
+					a_intern := (others => '0');
+					a_intern(count+19 downto count)	:= a_buf(19 downto 0);
+					z_intern := z_intern + a_intern;
+				end if;
+				count	:= count + 1;
+			elsif count = 20 then
+				z <= z_intern;
+				ready <= '1';
+			end if;
+		end if;
 	end process;
 
-	-- create golden device
-	gold_dev : process is
-	begin
-		wait for RESET_TIME;
-		loop
-			wait until ready = '1';
-			s_z_golden <= s_a * s_b;
-			wait for CYCLE_TIME;
-		end loop;
-	end process;
-
-	-- observe functionality
-	obs : process is
-	begin
-		wait for RESET_TIME;
-		loop
-			wait until ready = '1';
-			wait for CYCLE_TIME / 2;
-			assert s_z_golden = s_z
-			report "golden vector missmatch! end simulation!"
-			severity error;
-			wait for CYCLE_TIME / 2;
-		end loop;
-	end process;
-
-	-- create a clk driver here!
-	clk_process : process is
-	begin
-		clk <= '1';
-		wait for CYCLE_TIME *0.5;
-		clk <= '0';
-		wait for CYCLE_TIME *0.5;
-	end process;
-
-	-- create a reset driver here!
-	reset <= '1' , '0' after RESET_TIME;
-
-end testbench;
+end behave;
 
